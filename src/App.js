195d81131990bacacd1c65503cd4296b92c8870c
Tookai <71818808+Tookai@users.noreply.github.com>
2020-10-15 17:28:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./App.css";
 import SearchPage from "./components/SearchPage";
 
 function App() {
